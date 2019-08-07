31fab87ffb43538037794d87d6a2c2af3a0ea168
Esleiter Ramiro Bustamante Ataxca <esleiter-ramiro-bustamante@users.noreply.github.com>
2019-08-07 13:07:27 -0500

    Ejemplo

---
+++ target/classes/data.sql
@@ -0,0 +1,14 @@
+insert into student
+values(10001,'Ranga', 'E1234567');
+
+insert into student
+values(10002,'Ravi', 'A1234568');
+
+insert into student
+values(10003,'Eooo', 'A1134569');
+
+insert into student
+values(10004,'Oiroso', 'A1134570');
+
+insert into student
+values(10005,'Didi', 'A1134571');
